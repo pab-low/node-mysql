6e2a27b378ab72622fd384b200881a65187af761
pablo <pabloocampopita@gmail.com>
2019-07-08 13:29:51 -0400

    learn to make databases servers

--- API.js
+++ API.js
@@ -5,6 +5,7 @@ const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'admin',
   password : 'pablo',
+  database : 'blogss'
 });
 
 db.connect(function(err){
@@ -13,6 +14,73 @@ db.connect(function(err){
 });
 
 const api = express();
+// creating db
+api.get('/createdb',(req, res) => {
+    let sql = 'CREATE DATABASE blogss'
+    db.query( sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send('Database created ... ')
+    });
+});
+// create a table
+api.get("/createposttable", (req, res) => {
+    let sql = 'CREATE TABLE postsss (ID int NOT NULL AUTO_INCREMENT, title varchar(255), body varchar(255), PRIMARY KEY (ID))'
+    db.query( sql, (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("Table created ...");
+    });
+});
+// inserts a post into posts table
+api.get("/addpost", (req, res) => {
+    let post = {title: "My first Post", body: "Hello today was a good day"}
+    let sql = 'INSERT INTO postsss SET ?';
+    db.query( sql, post,  (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("first post add");
+    })
+});
+
+api.get("/secpost", (req, res) => {
+    let post = {title: "something", body: "Something else"}
+    let sql = 'INSERT INTO postsss SET ?';
+    db.query( sql, post,  (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send("add the second");
+    })
+});
+
+// select all post
+api.get("/getposts", (req, res) => {
+    let sql = 'SELECT * FROM postsss';
+    db.query( sql,  (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+//select a single post
+api.get("/getpost/:id", (req, res) => {
+    let sql = 'SELECT * FROM postsss WHERE ID =' + req.params.id;
+    db.query( sql,  (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
+
+api.get("/deletepost/:id", (req, res) => {
+    let sql = 'DELETE FROM postsss WHERE ID =' + req.params.id;
+    db.query( sql,  (err, result) => {
+        if (err) throw err;
+        console.log(result);
+        res.send(result);
+    })
+});
 
 
 
